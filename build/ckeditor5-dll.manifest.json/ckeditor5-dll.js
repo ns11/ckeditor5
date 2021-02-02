db1b0943ece24cc5dc0f910f631aeff07d425677
ns11 <alexander.cudreasov@gmail.com>
2021-02-02 07:27:29 +0100

    make the builds part of the repo

---
+++ build/ckeditor5-dll.manifest.json/ckeditor5-dll.js
@@ -0,0 +1,61 @@
+/*!
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+!function(e,r){"object"==typeof exports&&"object"==typeof module?module.exports=r():"function"==typeof define&&define.amd?define([],r):"object"==typeof exports?exports.dll=r():(e.CKEditor5=e.CKEditor5||{},e.CKEditor5.dll=r())}(window,(function(){return function(e){var r={};function n(o){if(r[o])return r[o].exports;var c=r[o]={i:o,l:!1,exports:{}};return e[o].call(c.exports,c,c.exports,n),c.l=!0,c.exports}return n.m=e,n.c=r,n.d=function(e,r,o){n.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,r){if(1&r&&(e=n(e)),8&r)return e;if(4&r&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var c in e)n.d(o,c,function(r){return e[r]}.bind(null,c));return o},n.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(r,"a",r),r},n.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},n.p="",n(n.s=0)}({"./src/clipboard.js":
+/*!**************************!*\
+  !*** ./src/clipboard.js ***!
+  \**************************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-clipboard'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-clipboard'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/clipboard\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/clipboard.js?")},"./src/cloud-services-core.js":
+/*!************************************!*\
+  !*** ./src/cloud-services-core.js ***!
+  \************************************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor-cloud-services-core'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor-cloud-services-core'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/cloud-services-core\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/cloud-services-core.js?")},"./src/core.js":
+/*!*********************!*\
+  !*** ./src/core.js ***!
+  \*********************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-core'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-core'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/core\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/core.js?")},"./src/engine.js":
+/*!***********************!*\
+  !*** ./src/engine.js ***!
+  \***********************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-engine'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-engine'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/engine\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/engine.js?")},"./src/enter.js":
+/*!**********************!*\
+  !*** ./src/enter.js ***!
+  \**********************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-enter'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-enter'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/enter\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/enter.js?")},"./src/paragraph.js":
+/*!**************************!*\
+  !*** ./src/paragraph.js ***!
+  \**************************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-paragraph'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-paragraph'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/paragraph\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/paragraph.js?")},"./src/select-all.js":
+/*!***************************!*\
+  !*** ./src/select-all.js ***!
+  \***************************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-select-all'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-select-all'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/select-all\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/select-all.js?")},"./src/typing.js":
+/*!***********************!*\
+  !*** ./src/typing.js ***!
+  \***********************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-typing'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-typing'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/typing\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/typing.js?")},"./src/ui.js":
+/*!*******************!*\
+  !*** ./src/ui.js ***!
+  \*******************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-ui'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-ui'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/ui\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/ui.js?")},"./src/undo.js":
+/*!*********************!*\
+  !*** ./src/undo.js ***!
+  \*********************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-undo'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-undo'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/undo\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/undo.js?")},"./src/upload.js":
+/*!***********************!*\
+  !*** ./src/upload.js ***!
+  \***********************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-upload'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-upload'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/upload\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/upload.js?")},"./src/utils.js":
+/*!**********************!*\
+  !*** ./src/utils.js ***!
+  \**********************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-utils'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-utils'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/utils\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/utils.js?")},"./src/widget.js":
+/*!***********************!*\
+  !*** ./src/widget.js ***!
+  \***********************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '@ckeditor/ckeditor5-widget'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nthrow new Error(\"Cannot find module '@ckeditor/ckeditor5-widget'\");\n/**\n * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.\n * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license\n */\n\n/**\n *  @module ckeditor5/widget\n */\n\n\n\n\n//# sourceURL=webpack://CKEditor5.dll/./src/widget.js?")},0:
+/*!****************!*\
+  !*** dll main ***!
+  \****************/
+/*! no static exports found */function(module,exports,__webpack_require__){eval("module.exports = __webpack_require__;\n\n//# sourceURL=webpack://CKEditor5.dll/dll_main?")}})}));
